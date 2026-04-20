c8bdc4fd9bc27ef34140a441941863f6516bd0fe
Andrei Belskii <mrandrewer@yandex.ru>
2026-04-21 00:38:24 +0300

    Add cake

---
+++ src/main/java/ru/hse/lab3/Cake.java
@@ -0,0 +1,94 @@
+package ru.hse.lab3;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * Класс, представляющий торт
+ */
+public class Cake extends ConfectioneryProduct {
+    /** Число слоев торта */
+    private int layers;
+
+    /**
+     * Создает {@link Cake} с названием "Торт", ценой 0 и одним слоем
+     */
+    public Cake() {
+        this("Торт", BigDecimal.ZERO, 1);
+    }
+
+    /**
+     * Создает торт с указанным названием, ценой и количеством слоев
+     *
+     * @param name   название торта
+     * @param price  цена торта
+     * @param layers количество слоев
+     * @throws IllegalArgumentException если layers меньше или равно нулю
+     */
+    public Cake(String name, BigDecimal price, int layers) {
+        super(name, price);
+        setLayers(layers);
+    }
+
+    /**
+     * Возвращает количество слоев торта
+     *
+     * @return количество слоев
+     */
+    public int getLayers() {
+        return layers;
+    }
+
+    /**
+     * Устанавливает количество слоев торта
+     *
+     * @param layers количество слоев
+     * @throws IllegalArgumentException если layers меньше или равно нулю
+     */
+    public void setLayers(int layers) {
+        if (layers <= 0) {
+            throw new IllegalArgumentException("Number of layers must be positive");
+        }
+        this.layers = layers;
+    }
+
+    /**
+     * Возвращает строковое представление торта
+     *
+     * @return строка с названием, ценой и количеством слоев
+     */
+    @Override
+    public String toString() {
+        return String.format("Cake{name='%s', price=%.2f, layers=%d}", getName(), getPrice(), layers);
+    }
+
+    /**
+     * Сравнивает торт с другим объектом
+     *
+     * @param obj объект для сравнения
+     * @return true, если объект является тортом с теми же свойствами
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Cake other)) {
+            return false;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        return layers == other.layers;
+    }
+
+    /**
+     * Возвращает хеш-код торта
+     *
+     * @return хеш-код на основе родительского объекта и количества слоев
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), layers);
+    }
+}
---
+++ src/main/java/ru/hse/lab3/ConfectioneryProduct.java
@@ -0,0 +1,116 @@
+package ru.hse.lab3;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * Базовый класс кондитерских изделий
+ */
+public class ConfectioneryProduct {
+    /** Название кондитерского изделия */
+    private String name;
+
+    /** Цена изделия */
+    private BigDecimal price;
+
+    /**
+     * Создает {@link ConfectioneryProduct} с значениями по умолчанию
+     * Название устанавливается как "Кондитерское изделие", цена равна 0
+     */
+    public ConfectioneryProduct() {
+        this("Кондитерское изделие", BigDecimal.ZERO);
+    }
+
+    /**
+     * Создает {@link ConfectioneryProduct} с указанными названием и ценой
+     *
+     * @param name  название изделия
+     * @param price цена изделия
+     * @throws IllegalArgumentException если название пустое или цена отрицательная
+     */
+    public ConfectioneryProduct(String name, BigDecimal price) {
+        setName(name);
+        setPrice(price);
+    }
+
+    /**
+     * Возвращает название изделия
+     *
+     * @return название изделия
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Устанавливает название изделия
+     *
+     * @param name название изделия
+     * @throws IllegalArgumentException если название пустое или null
+     */
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name.trim();
+    }
+
+    /**
+     * Возвращает цену изделия
+     *
+     * @return цена изделия
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * Устанавливает цену изделия
+     *
+     * @param price цена изделия
+     * @throws IllegalArgumentException если цена отрицательная
+     */
+    public void setPrice(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price = price;
+    }
+
+    /**
+     * Возвращает строковое представление кондитерского изделия
+     *
+     * @return строка с названием и ценой изделия
+     */
+    @Override
+    public String toString() {
+        return String.format("ConfectioneryProduct{name='%s', price=%.2f}", name, price);
+    }
+
+    /**
+     * Сравнивает текущее изделие с другим объектом
+     *
+     * @param obj объект для сравнения
+     * @return true, если объекты одного класса и имеют одинаковые название и цену
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof ConfectioneryProduct other)) {
+            return false;
+        }
+        return price.compareTo(other.price) == 0 && Objects.equals(name, other.name);
+    }
+
+    /**
+     * Возвращает хеш-код изделия.
+     *
+     * @return хеш-код на основе названия и цены
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
